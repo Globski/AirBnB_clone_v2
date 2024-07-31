58b4df1ddcfbdb9693e87edccfb04c49f8ed3999
Gloriana <141042662+Globski@users.noreply.github.com>
2024-07-31 11:33:09 -0700

    Add Flask route for number odd or even check in the body with HTML template.

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -63,12 +63,7 @@ def number_odd_or_even(n):
     """Displays an HTML page only if <n> is an integer
     States whether <n> is odd or even in the body.
     """
-    return render_template(
-        "6-number_odd_or_even.html",
-        n=n,
-        odd_or_even="odd" if n % 2 != 0 else "even"
-        )
-
+    return render_template("6-number_odd_or_even.html", n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
