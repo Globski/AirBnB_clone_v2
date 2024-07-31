d6edc7900ad5cd5d65056f22475fe5cc3c124e37
globski <gloriaogunsemore@gmail.com>
2024-07-31 17:39:52 +0000

    This route handles both 'text' and integer parameters. It ensures 'text' replaces underscores with spaces and 'number' is displayed as a number.

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: Displays 'HBNB'
+    /c/<text>: Displays 'C <text>'
+    /python/(<text>): Displays 'Python <text>'
+    /number/<n>: Displays '<n> is a number' if <n> is an integer
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Displays 'C <text>'
+    Replace underscores with spaces in <text>
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Displays 'Python <text>'
+    Replace underscores with spaces in <text>
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    """Displays '<n> is a number' if <n> is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
