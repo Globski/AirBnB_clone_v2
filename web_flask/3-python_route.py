6f69755d653b75d5bfb9de83d3aefd5d042e58bc
globski <gloriaogunsemore@gmail.com>
2024-07-31 17:34:43 +0000

    This task introduces a new route that captures a variable 'text' from the URL and displays 'C <text>', with underscores in 'text' replaced by spaces.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: Displays 'HBNB'
+    /c/<text>: Displays 'C <text>'
+    /python/(<text>): Displays 'Python <text>'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Displays 'C <text>'
+    Replace underscores with spaces in <text>
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Displays 'Python <text>'
+    Replace underscores with spaces in <text>
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
