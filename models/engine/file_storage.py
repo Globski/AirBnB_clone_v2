8f9994053ffa222b74323c1b1abe8fced77fbe00
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-08-27 11:56:42 +0000

    delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,10 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes an object from storage"""
+        if obj:
+            key = f"{obj.to_dict()['__class__']}.{obj.id}"
+            if key in FileStorage.__objects:
+                del FileStorage.__objects[key]
